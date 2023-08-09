38b665124eb09c075f8f6e9064cd3d3ddab72ae4
AlexandrAguilarC <alexandra10aguilarc@gmail.com>
2023-08-09 10:54:26 -0400

    Se utiliza el archivo seed para ingresar información base en cada modelo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Creating 10 movies...'
+10.times do |i|
+    Movie.create(name: "Name #{i}", sypnosis: "Sypnosis #{i}", director: "Director #{i}")
+end
+
+puts 'Creating 10 series...'
+10.times do |i|
+    Serie.create(name: "Name #{i}", sypnosis: "Sypnosis #{i}", director: "Director #{i}")
+end
+
+puts 'Creating 10 documentaries...'
+10.times do |i|
+    DocumentaryFilm.create(name: "Name #{i}", sypnosis: "Sypnosis #{i}", director: "Director #{i}")
+end
